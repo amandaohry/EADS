37f18de7d7cd9dc82f00e899567957a22031ed7c
Amanda Oh <amandaohry@gmail.com>
2019-04-07 14:59:01 +0800

    getServiceDetail()

--- src/java/dao/CurrentTimeDAO.java
+++ src/java/dao/CurrentTimeDAO.java
@@ -9,9 +9,6 @@ package dao;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 /**
